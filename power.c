95f58d6ce0ed70ad3db96d40c1b5a40faf4e7166
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-03-01 12:06:18 +0530

    incomp q9

---
+++ power.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    /*for (a=1; a > 0; a++)
+    {
+        a=b;
+        printf("%d""%d",a*b);
+    }
+    */
+ //take input from the user and return it's power
+ printf("Enter x ");
+ scanf("%d",&a);
+printf("Enter y ");
+scanf("%d",&b);
+
+
+}
